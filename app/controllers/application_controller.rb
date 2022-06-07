00c2375104ca9318f851044e11a13f28e25fec7f
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-07 12:58:40 +0000

    データが保存できるよう許可の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # 下記のように記述することで、devise利用の機能(ユーザー登録、ログイン認証など)が使用される前にconfigure_...メソッドが実行される。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # protectedは呼び出された他のコントローラからも参照することが出来る。
+  protected
+  
+  # configure_...メソッドでは、devise_...メソッドを使うことでユーザー登録の際、ユーザー名のデータ操作を許可しています。
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
 end
