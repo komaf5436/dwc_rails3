ea604c1d6845a32c3183b702e059f7824a4310e4
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-07 01:20:28 +0000

    デフォルトページ設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  # root to:と記述することで、/にアクセスした場合、自分の作成したページに設定することが出来る。
+  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
