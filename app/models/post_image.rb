fcf56f0973c79b3cc4d8aaec651a01d5d373484a
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-08 04:09:30 +0000

    アソシエーション設定

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,7 @@
 class PostImage < ApplicationRecord
-  
+
   has_one_attached :image
+
+  # PostImageは、Userに属しているという関係からPostImageモデルでは下記のように記述する。
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # UserモデルとPostImageモデルを関連付けるため下記のように記述する
+  # User１つに対して、PostImageを複数個持っているという1:Nの関係になっているので下記のようになる。
+  has_many :post_images, dependent: :destroy
 end
