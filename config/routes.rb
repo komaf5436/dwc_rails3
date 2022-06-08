8ed49b84310c73736ff3e2c3408716e9ea4c52c2
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-08 02:31:19 +0000

    aboutページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # root to:と記述することで、/にアクセスした場合、自分の作成したページに設定することが出来る。
   root to: "homes#top"
+  get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
