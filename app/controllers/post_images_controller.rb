6110e540759d0b2164cc7e4930508e57017508a6
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-09 13:55:30 +0000

    ヘッダーに投稿フォームを追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,17 +1,17 @@
 class PostImagesController < ApplicationController
   def new
-    @post_iamge = PostImage.new
+    @post_image = PostImage.new
   end
-  
+
   # 投稿データの保存
   def create
     # PostImageモデルに紐づくデータとして保存する準備をし、フォームに入力されたデータが、@post_imageに格納される
-    @post_iamge = PostImage.new(post_image_params)
+    @post_image = PostImage.new(post_image_params)
     # @post_image.user_idは、この投稿のuser_idとしてcurrent_user.idの値を代入するという意味
-    # current_userは、ログイン中のユーザー情報を取得することが出来る。この場合、ログインユーザーのidを取得することが出来る
+    # current_userは、ログイン中のユーザー情報を取得することが出来る。この場合、ログインユーザーのidを取得することが出来る。
     @post_image.user_id = current_user.id
-    @post_iamge.save
-    redirect_to post_iamge_path
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -19,13 +19,13 @@ class PostImagesController < ApplicationController
 
   def show
   end
-  
+
   # 投稿データのストロングパラメータ
   private
-  
+
   # フォームで入力されたデータが、投稿データとして許可されているパラメータかどうかをチェック
   def post_image_params
-    params.require(:post_iamge).permit(:shop_name, :image, :caption)
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
