2f5227a071fe72f7fbc08dedf55363dc4b9cbbc1
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-09 13:43:12 +0000

    ルーティングにcreateを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # resourcesメソッドは、ルーティングを一括して自動生成してくれる機能。
   # onlyオプションを使用することで、生成するルーティングを限定してくれる。
   # 今回の場合、new,index,showのアクション以外は、ルーティングは行われない。
-  resources :post_images, only:[:new, :index, :show]
+  resources :post_images, only:[:new, :create, :index, :show]
   
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
