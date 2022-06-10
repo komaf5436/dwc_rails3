bd60570dc69b110079d33739d9fa9467d6393a0a
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-10 01:45:12 +0000

    画像表示の記述

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,4 +4,14 @@ class PostImage < ApplicationRecord
 
   # PostImageは、Userに属しているという関係からPostImageモデルでは下記のように記述する。
   belongs_to :user
+  
+  # get_imageメソッドをPostImageモデルの中に記述することで、カラムを呼び出すようにこの処理(メソッド)を呼び出すことが出来る。
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+  
 end
