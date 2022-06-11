cbc67e64b916faf725817861963a59259fb1b0c8
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-11 08:06:44 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   # root to:と記述することで、/にアクセスした場合、自分の作成したページに設定することが出来る。
   root to: "homes#top"
-  
+  get 'homes/about' => 'homes#about', as: 'about'
   # resourcesメソッドは、ルーティングを一括して自動生成してくれる機能。
   # onlyオプションを使用することで、生成するルーティングを限定してくれる。
   # 今回の場合、new,create,index,showのアクション以外は、ルーティングは行われない。
   resources :post_images, only:[:new, :create, :index, :show, :destroy]
-  
-  get 'homes/about' => 'homes#about', as: 'about'
+  resources :users, only:[:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
