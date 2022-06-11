9216f63ba6084a4dd4da61de5e47975ae342a99c
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-11 07:54:34 +0000

    Userモデルにプロフィール画像保存の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
   # UserモデルとPostImageモデルを関連付けるため下記のように記述する
   # User１つに対して、PostImageを複数個持っているという1:Nの関係になっているので下記のようになる。
   has_many :post_images, dependent: :destroy
+
+  # 下記の記述により、profile_imageという名前でActiveStorageでプロフィール画像を保存できるよう設定する。
+  has_one_attached :profile_image
+  
+  # PostImageモデルで記述した内容と変わらないが、下から2行目のコードで画像サイズの変更を行なっている。
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [100,100]).processed
+  end
+  
 end
