56fc2e9269f6d1dcf1bdc029193c7c0e5a7e8bb6
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-11 12:59:53 +0000

    マイページリンクを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # 下記のように記述することで、特定のユーザ(@user)に関連付けられた投稿全て(.post_images)を取得し@post_imagesに渡すという処理を行う。
-    @post_image = @user.post_images
+    @post_images = @user.post_images
   end
 
   def edit
