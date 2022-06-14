d89e0c103578da1965642fd258596f0a1a851826
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-14 16:04:35 +0000

    更新機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # onlyオプションを使用することで、生成するルーティングを限定してくれる。
   # 今回の場合、new,create,index,showのアクション以外は、ルーティングは行われない。
   resources :post_images, only:[:new, :create, :index, :show, :destroy]
-  resources :users, only:[:show, :edit]
+  resources :users, only:[:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
