d23a850a408575264c1630df701748846cd8b10d
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-19 02:21:33 +0000

    21章　権限の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  # ログイン認証が済んでいない状態でトップページ以外の画面にアクセスしてもログイン画面へリダイレクトするようになる。
+  # authenticate_userメソッドは、devise側のメソッドで、下記のように記述することでログイン認証されていなければ、ログイン画面へリダイレクトするという機能を実装する。
+  # exceptは指定したアクションをbefore_actionの対象から外す。今回の場合、トップページのみログイン状態に関わらず、アクセス可能となる。
+  before_action :authenticate_user!, except:[:top]
+  
   # 下記のように記述することで、devise利用の機能(ユーザー登録、ログイン認証など)が使用される前にconfigure_...メソッドが実行される。
   before_action :configure_permitted_parameters, if: :devise_controller?
 
